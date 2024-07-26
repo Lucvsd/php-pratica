bc8a2fb1811cefdcc05791b06f51c03307b71ab3
Lucas Pontes <89760771+Lucvsd@users.noreply.github.com>
2024-07-26 16:09:38 -0300

    Add files via upload

---
+++ ifeslse_operadores_condicionais.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Curso PHP</title>
+    </head>
+    
+    <body>
+        <?php 
+
+            //== igaul
+            //=== identico
+            //!= ou <> diferentes
+            //!== não identico 
+            //< menor que o valor a  direita ?
+            //> valor da esquerta é maior ?
+            //<= menor ou igual
+            //>= maior ou igual
+
+            if(6 >= 6){
+                echo 'verdadeiro';
+
+            } else()
+                echo 'falso';
+        
+        ?>
+
+
+    </body>
+</html>
