189da0cb0d5b2be020a9256a83fa1eef891aef86
Lucas Pontes <89760771+Lucvsd@users.noreply.github.com>
2024-07-26 18:55:58 -0300

    Add files via upload

---
+++ operadores_aritimeticos.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Curso PHP</title>
+    </head>
+    
+    <body>
+        <?php 
+
+            $num1 = 8;
+            $num2 = 4;
+
+            echo "A soma entre $num1 e $num2 é" . ($num1 + $num2);
+            echo '<br />';
+            echo "A subtração entre $num1 e $num2 é" . ($num1 - $num2);
+            echo '<br />';
+            echo "A mutiplicação entre $num1 e $num2 é" . ($num1 * $num2);
+            echo '<br />';
+            echo "A divisão entre $num1 e $num2 é" . ($num1 / $num2);
+            echo '<br />';
+            echo "A módulo entre $num1 e $num2 é" . ($num1 % $num2);
+            echo '<br />';
+
+            //8 / 4
+            //2 / 8
+
+        ?>
+    </body>
+</html>
