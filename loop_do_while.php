9f4d975fa476801b433dadace2f11d71a06e6bcb
Lucas Pontes <89760771+Lucvsd@users.noreply.github.com>
2024-07-28 15:43:39 -0300

    Add files via upload

---
+++ loop_do_while.php
@@ -0,0 +1,26 @@
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Curso PHP</title>
+</head>
+
+<body>
+    <?php
+
+        $x = 1;
+        
+        do {
+            echo "X = $x <br />"
+
+            $x++; //criterio de parada
+
+            //continue
+            //break
+        } while($x < 9);
+        
+
+    ?>
+</body>
+
+</html>
---
+++ loop_for.php
@@ -0,0 +1,35 @@
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Curso PHP</title>
+</head>
+
+<body>
+    <?php
+
+        /* 
+        $x = 1;
+        
+        while($x < 10) {
+        
+        }
+
+        do { 
+        
+        } while($x < 10);
+        */
+
+        for($x = 10; $x > 0; $x--) {
+
+            //break
+            //continue
+
+            echo "$x <br />";
+
+        }
+
+    ?>
+</body>
+
+</html>
