ac9dc18b9fda88ecc39c7628fcf4b34eebe32d55
Lucas Pontes <89760771+Lucvsd@users.noreply.github.com>
2024-07-28 14:53:20 -0300

    Add files via upload

---
+++ loops_while.php
@@ -0,0 +1,38 @@
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Curso PHP</title>
+</head>
+
+<body>
+    <?php
+
+        $num = 1;
+
+        //operadores comparação / lógicos 
+        echo '-- Início do loop --<br />';
+
+        while($num < 10) {
+
+            $num++; //critério de parada 
+
+            if($num == 2 || $num == 6) {
+                continue
+            }
+
+            echo "$num <br />";
+
+            /*
+            if($num > 100) {
+                break;
+            }
+            */ 
+
+        }
+
+        echo '-- Fim do loop --';
+    ?>
+</body>
+
+</html>
